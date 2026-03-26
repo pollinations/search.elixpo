4921438abb434495516003c3b70007446095c44a
Circuit-Overtime <ayushbhatt633@gmail.com>
2026-03-26 13:31:36 +0000

    Create the Python client SDK package.

---
+++ lix_open_search/lix_open_search/__init__.py
@@ -0,0 +1,20 @@
+from lix_open_search.client import LixSearch, AsyncLixSearch
+from lix_open_search.models import (
+    SearchResult,
+    Message,
+    Session,
+    SurfResult,
+    StreamChunk,
+)
+
+__all__ = [
+    "LixSearch",
+    "AsyncLixSearch",
+    "SearchResult",
+    "Message",
+    "Session",
+    "SurfResult",
+    "StreamChunk",
+]
+
+__version__ = "0.1.0"
